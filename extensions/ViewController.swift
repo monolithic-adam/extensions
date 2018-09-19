751ef3a70c2754a09e6db3e786721e483a6dd3c1
ahenry <adam.henry@up-frontier.jp>
2018-09-19 11:44:38 +0900

    - create base example project - remove unnecessary gitignore parts

---
+++ extensions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  extensions
+//
+//  Created by ahenry on 2018/09/19.
+//  Copyright © 2018 monolithic-adam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
